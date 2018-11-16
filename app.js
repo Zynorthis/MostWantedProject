1f5af611103f0a75de316d785ff272312f22b172
unknown <megdrescher@icloud.com>
2018-11-16 11:24:30 -0600

    Edited eyeColor function and prompt

--- app.js
+++ app.js
@@ -157,7 +157,7 @@ function age(input){
 
 //for validation of gender -M
 function gender(input, personGender){
-  let genderM=promptFor("Please Enter Gender");
+  let genderM=promptFor("Please Enter Gender" );
   let genderF=promptFor("Please Enter Gender");
   if (genderM.checked==false && genderF.checked==false)
       { 
@@ -167,7 +167,7 @@ function gender(input, personGender){
 }
 //for validation of height-M
 function height(input, personHeight){
-  let personHeight=promptFor("Please Enter Person's Height.");
+  let personHeight=promptFor("Please Enter Person's Height.",height);
   if(height_ft=="" || height_in =="" || isNan(height_ft) || isNaN(height_in)){
   alert("Please enter height, must be numerical!");
         return false;
@@ -175,16 +175,22 @@ function height(input, personHeight){
 }
 //validation of weight-M
 function weight(input, personWeight) {
-  let personWeight=promptFor("Please Enter Person's Weight.");
+  let personWeight=promptFor("Please Enter Person's Weight.",weight);
   if(weight =="" || isNan(weight)){
     alert ("Please enter weight, must be numerical!");
       return false;
   }
 }
 //validation of eyecolor-M
-function eyeColor(input, personEyeColor){
-  let personEyeColor=promptFor("Please Choose Person's Eye Color")
-
+function eyeColor(input, personEyeColor,peopleEyeColor){
+  let personEyeColor=promptFor("Please Choose Person's Eye Color",eyeColor);
+  let peopleEyeColor=eyeColor.push(["Brown", "Black","Hazel","Blue","Green"]);
+  for (i=0, personEyeColor; personEyeColor=peopleEyeColor[i++];){
+  if (personEyeColor =="" () ){
+    alert("Please Enter A Valid Color!");
+      return false;
+    }
+  }
 }
 
 
